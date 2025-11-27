b869ef9d1d542cdf8fad270405353182a69acdf0
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-11-27 12:27:37 +0100

    Added dice thrower

---
+++ src/components/DiceThrower.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+
+const diceCount = ref({
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+});
+
+function throwDice() {
+    diceCount.value = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0};
+    for (let i = 0; i < 8; i++) {
+        const result = Math.floor(Math.random() * 6) + 1;
+        diceCount.value[result]++;
+    }
+}
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Getal</th>
+                <th>Aantal</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(face, index) in diceCount" :key="index">
+                <td>{{ index }}</td>
+                <td>{{ face }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <button @click="throwDice">Gooi!</button>
+</template>
+
+<style scoped>
+</style>
