3b146efc7f8ebef2a6280b0d48e112aae24cea52
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-11-27 13:24:49 +0100

    Update DiceThrower.vue

--- src/components/DiceThrower.vue
+++ src/components/DiceThrower.vue
@@ -10,7 +10,7 @@ const diceCount = ref({
     6: 0
 });
 
-function throwDice() {
+const throwDice = () => {
     diceCount.value = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0};
     for (let i = 0; i < 8; i++) {
         const result = Math.floor(Math.random() * 6) + 1;
